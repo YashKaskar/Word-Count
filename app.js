2b491e02a2c6cacc3911de7707efdbcad2a61726
Yash Kaskar <yash.kaskar99@gmail.com>
2022-11-10 11:10:42 +0530

    document .querySelector

---
+++ app.js
@@ -0,0 +1,3 @@
+const wordsTextArea = document.querySelector('.container .word');
+const countbtn = document.querySelector('.container .count-btn');
+const wordCount = document.querySelector('.container .word-count span');
