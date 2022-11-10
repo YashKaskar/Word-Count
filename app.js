d484e7c54e3005b3f979aa85c07ff6967cab66a7
Yash Kaskar <yash.kaskar99@gmail.com>
2022-11-10 11:35:54 +0530

    js

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 const wordsTextArea = document.querySelector('.container .word');
 const countbtn = document.querySelector('.container .count-btn');
 const wordCount = document.querySelector('.container .word-count span');
+
+const countWords = () => {
+    let words = wordsTextArea.value;
+    let wordsTrimmed = words.replace(/\s+/g, '').trim()
+    let splitWords = wordsTrimmed.split('');
+    let numberofWords = splitWords.length;
+    if (splitWords[0] = '') {   
+        numberofWords = 0
+    }
+    wordCount.innerText = numberofWords;
+}
+
+countbtn.addEventListener('click', countWords)
+
